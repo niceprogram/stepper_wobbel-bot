bf11c3193c8e428d9733d57020ce2e29eb3ffc85
K_Van <niceprogram@yahoo.com>
2017-12-30 01:04:00 +0100

    Update wobbel-bot.py

--- wobbel-bot.py
+++ wobbel-bot.py
@@ -17,7 +17,8 @@ GPIO.setmode(GPIO.BCM)
 # Define the GPIO pins
 Set_motorA_pins = [6,13,19,26]
 Set_motorB_pins= [12,16,20,21]
-
+OldA = 0;
+OldB = 0;
 
 # Set all pins as output
 for pinA in Set_motorA_pins:
@@ -37,60 +38,102 @@ for pinB in Set_motorB_pins:
 Delay_ = 0.5 # for 8 sequence .0009
 
 # this function...
-def motor_block():
+def motor_block(directionA, directionB,wheel_turns,speed):
+	global Delay_
 	StepCounter= 0
-	Four_or_eight_steps=0
-	while Four_or_eight_steps <= 8:
-		for pinB in range(0, 4):
-			xpin = Set_motorB_pins[pinB]
-			if Seq[StepCounter][pinB]!=0:
-			  print " Step %i Enable %i" %(StepCounter,xpin)
-			  GPIO.output(xpin, True)
+	
+	# set speed to zero if the wheel direction changes
+	if (directionA != OldA OR directionB != OldB):
+		Delay_ =0.5
+	OldA = directionA
+	OldB = directionB
+	
+	
+	if (speed < Delay_):
+		Delay_ = 0.5 #wheel needs to speed up 
+		Speedup = -1
+	else: 	
+		Delay_ = speed # wheel slow down
+		Speedup = 1
+	
+	# wheel squece	
+	if  (directionA == -1):
+		Seq_MPA = Seq[::-1]
+	elif (directionA == 1):
+		Seq_MPA = Seq
+	else: 	
+		Seq_MPA = stepper_off
+		
+	
+	if  (directionB == -1):
+		Seq_MPB = Seq[::-1]
+	elif (directionB == 1):
+		Seq_MPB = Seq
+	else: 	
+		Seq_MPB = stepper_off
+	
+		
+		
+	while wheel_turns <= 8*2000:
+		for pin in range(0, 4):
+			xpinB = Set_motorB_pins[pin]
+			if Seq_MPB[StepCounter][pin]!=0:
+			  print " BStep %i Enable %i" %(StepCounter,pin)
+			  GPIO.output(xpinB, True)
 			else:
-			  GPIO.output(xpin, False)
+			  GPIO.output(xpinB, False)
+			xpinA = Set_motorA_pins[pin]
+			if Seq_MPA[StepCounter][pin]!=0:
+			  print " AStep %i Enable %i" %(StepCounter,pin)
+			  GPIO.output(xpinA, True)
+			else:
+			  GPIO.output(xpinA, False)
+			
 		StepCounter += 1
+		
 		if (StepCounter==StepCount):
 			StepCounter = 0
 		if (StepCounter<0):
 			StepCounter = StepCount
+		
+		if (Speedup == -1 AND Delay_>sleep): # speed up
+			Delay_ = Delay_ + (Speedup*0.00001)
+		elif (Speedup == 1 AND speed>Delay_): # speed slow down
+			Delay_ = Delay_ + (Speedup*0.00001)
+		
+		else:
+			Delay_ = sleep
+		
 		time.sleep(Delay_)
-	Four_or_eight_steps+=1
+	
+	
+	wheel_turns +=1
 	
 	
 
 
 # stepper_code8 sequence
-stepper_code8= []
-stepper_code8= range(0, 8)
-stepper_code8[0] = [1,0,0,0]
-stepper_code8[1] = [1,1,0,0]
-stepper_code8[2] = [0,1,0,0]
-stepper_code8[3] = [0,1,1,0]
-stepper_code8[4] = [0,0,1,0]
-stepper_code8[5] = [0,0,1,1]
-stepper_code8[6] = [0,0,0,1]
-stepper_code8[7] = [1,0,0,1]
-stepper_off = [0,0,0,0]
+Seq= []
+Seq= range(0, 8)
+Seq[0] = [1,0,0,0]
+Seq[1] = [1,1,0,0]
+Seq[2] = [0,1,0,0]
+Seq[3] = [0,1,1,0]
+Seq[4] = [0,0,1,0]
+Seq[5] = [0,0,1,1]
+Seq[6] = [0,0,0,1]
+Seq[7] = [1,0,0,1]
 
-# stepper_code4 sequence
-stepper_code4= []
-stepper_code4= range(0, 4)
-stepper_code4[0] = [1,0,0,0]
-stepper_code4[1] = [0,1,0,0]
-stepper_code4[2] = [0,0,1,0]
-stepper_code4[3] = [0,0,0,1]
+stepper_off = [0,0,0,0]*8
 
-# Choose a sequence to use
-Seq = stepper_code8
-#Seq.reverse()
-StepCount = 8
+
 
 
 
 # Start main loop
 #while 1==1:
 
-motor_block()
+motor_block(1,1,3,0.0005)
  # for pinB in range(0, 4):
   #  xpin = Set_motorB_pins[pinB]
   #  if Seq[StepCounter][pinB]!=0:
