af3c6cdca482fd6279beb321087e90fefd8e0a15
K_Van <niceprogram@yahoo.com>
2018-01-13 11:41:56 +0100

    Update wobbel-bot.py

--- wobbel-bot.py
+++ wobbel-bot.py
@@ -7,59 +7,66 @@
 #-----------------------------------
 #!/usr/bin/env python
 
-# Import required libraries
+#Import required libraries
 import time
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
-# GPIO references
-#GPIO.setmode(GPIO.BCM)
+#GPIO references
+GPIO.setmode(GPIO.BCM)
 
 # Define the GPIO pins
 Set_motorA_pins = [6,13,19,26]
 Set_motorB_pins= [12,16,20,21]
 OldA = 0;
 OldB = 0;
+#speed 
+bottom_value = 9   #/10000  0.0009   
+top_value =  500   #/10000  0.05      
+speedrange = (top_value-bottom_value)
+Old_speed = 0
 
 # Set all pins as output
 for pinA in Set_motorA_pins:
   print "Setup pins A"
-  #GPIO.setup(pinA,GPIO.OUT)
-  #GPIO.output(pinA, False)
+  GPIO.setup(pinA,GPIO.OUT)
+  GPIO.output(pinA, False)
   
 for pinB in Set_motorB_pins:
   print "Setup pins B"
-  #GPIO.setup(pinB,GPIO.OUT)
-  #GPIO.output(pinB, False)
+  GPIO.setup(pinB,GPIO.OUT)
+  GPIO.output(pinB, False)
  
 # Define variables
-# StepCounter= 0
-#Delay_ = 0.002  # for 4 sequence
 
-Delay_ = 0.05 # for 8 sequence .0009
 
-# this function...
-def motor_block(directionA, directionB,wheel_turns,speed):
-	global Delay_
-	global OldA
+
+
+# this function get the motor to turn
+def motor_block(directionA, directionB,wheel_turns,speed_percent):
+	global Seq_MPA
+	global Seq_MPB
+	global OldA			#old determines if the wheel has changed direction
 	global OldB
-	StepCounter = 0
-	StepCount = 8
+	global Old_speed
+
+	Wheel = 0
 	
-	# set speed to zero if the wheel direction changes
+	
+	# set speed_percent to zero if the wheel direction changes
 	if (directionA != OldA or directionB != OldB):
-		Delay_ =0.05
+		speed_percent = 0
+	
 	OldA = directionA
 	OldB = directionB
 	
 	
-	if (speed < Delay_):
-		Delay_ = 0.05 #wheel needs to speed up 
-		Speedup = -1
-	else: 	
-		Delay_ = speed # wheel slow down
+	if (speed_percent < Old_speed):
 		Speedup = 1
+	else: 	
+		Speedup = -1 # wheel slow down
+		
 	
-	# wheel squece	
+	# wheel squence [0001]	
 	if  (directionA == -1):
 		Seq_MPA = Seq[::-1]
 	elif (directionA == 1):
@@ -75,51 +82,58 @@ def motor_block(directionA, directionB,wheel_turns,speed):
 	else: 	
 		Seq_MPB = stepper_off
 	
+    #slow down  from 100%-0% in half a turn
+	if (speed_percent > Old_speed):
+		Speedup = -1
+		ramp = (Old_speed - speed_percent)   #100-25 = 75 change speed in 4096/2 steps
+		current_speed = Old_speed
+		while Wheel <= wheel_turns and current_speed > speed_percent: #tagret speed
+		    speed_value = (((100-speed_percent)*speedrange/100)+bottom_value)/10000
+			turn_wheel(0.005)  #speed_value
+			current_speed -=1
+		Wheel +=1
+	
+	#speed up
+	else: 	
+		Speedup = -1 # wheel slow down	
+	
+	
+
+	
+	
+	#constant speed
+
+	
+	
 		
-		
-	while wheel_turns <= 8*2000:
-		for pin in range(0, 4):
-			xpinB = Set_motorB_pins[pin]
-			if Seq_MPB[StepCounter][pin]!=0:
-			  print " BStep %i Enable %i" %(StepCounter,pin)
-			  #GPIO.output(xpinB, True)
-			else:
-			  #GPIO.output(xpinB, False)
-			  xpinA = Set_motorA_pins[pin]
-			if Seq_MPA[StepCounter][pin]!=0:
-			  print " AStep %i Enable %i" %(StepCounter,pin)
-			  #GPIO.output(xpinA, True)
-			else:
-			  pass  
-			  #GPIO.output(xpinA, False)
+def turn_wheel(Delay_):
+	StepCounter = 0	
+	for pin in range(0, 4):
+		xpinB = Set_motorB_pins[pin]
+		if Seq_MPB[StepCounter][pin]!=0:
+			print " BStep %i Enable %i" %(StepCounter,pin)
+			GPIO.output(xpinB, True)
+				  
+		else:
+			GPIO.output(xpinB, False)
+			xpinA = Set_motorA_pins[pin]
 			
-    		StepCounter += 1
-    		
-    		if (StepCounter==StepCount):
-    			StepCounter = 0
-    		if (StepCounter<0):
-    			StepCounter = StepCount
-    		
-    		if (Speedup == -1 and Delay_>speed): # speed up
-    			Delay_ = Delay_ + (Speedup*0.001)
-    		elif (Speedup == 1 and  speed>Delay_): # speed slow down
-    			Delay_ = Delay_ + (Speedup*0.001)
-    		else:
-    			Delay_ = speed
-    			Speedup = 0
-    			
-    		if Delay_ < 0.05:
-    		  Delay_ = speed
-    		  Speedup = 0
-    		print Speedup	
-    		print Delay_
-    		time.sleep(Delay_)
+		if Seq_MPA[StepCounter][pin]!=0:
+			print " AStep %i Enable %i" %(StepCounter,pin)
+			GPIO.output(xpinA, True)
+		else:
+			pass  
+			GPIO.output(xpinA, False)
+				
+		StepCounter += 1
+				
+		time.sleep(Delay_)
+		
+		
 	
 	
-	wheel_turns +=1
 	
 	
-	wheel_turns +=1
 	
 	
 
@@ -144,8 +158,14 @@ stepper_off = [0,0,0,0]*8
 
 # Start main loop
 #while 1==1:
+# motor_block (A,B, turns, speed)
+#motor_block(1,1,2,0.0009)
+#motor_block(-1,-1,3,0.0009)
+motor_block(1,1,2,100)
+#motor_block(1,1,2,0.10)
+#motor_block(1,-1,5,0.0009)
+#motor_block(-1,1,5,0.0009)
 
-motor_block(1,1,3,0.0005)
  # for pinB in range(0, 4):
   #  xpin = Set_motorB_pins[pinB]
   #  if Seq[StepCounter][pinB]!=0:
